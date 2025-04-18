765b327698f366319ee1bbdb4410cc696eeed0fb
luulintran <luulitran@gmail.com>
2025-04-18 13:06:37 -0600

    Reorganized 02_diffbind_e16.R
    Reorganized script, moving code that loads libraries to the set_up_environment.R script. Created a config.R file for each fig, so also moved any input files, output dirs, filenames, etc to the config.R script so that everything can be modified here instead.

--- analysis/02_diffbind_e16.R
+++ analysis/02_diffbind_e16.R
@@ -1,43 +1,60 @@
-# SET UP
-library(DiffBind)
-library(tidyverse)
-library(dplyr)
-library(readr)
+# DEFINE FILES AND PATHS: ------------------------------------------------------
+# Path to the gene raw counts matrix
+input_file <- "data/meta_data/atacseq_e16/SampleSheet.csv"
 
-# Set working directory to project
-# setwd("..")
+# Path to output directory for r_objects
+output_dir_robj <- "data/processed_data/atacseq_e16/r_objects"
 
-# READ IN DATA:------------------------------------------------------------------------------------------
+# filename for RDS file containing normalized counts
+filename_normcounts <- "norm_read_counts_dbObj.rds"
+
+# filename for RDS file containing DBA object after differential analysis
+filename_dbObj <- "diffbind_dbObj.rds"
+
+# Make sure output directory exists, and if it doesn't, create one
+if (!dir.exists(output_dir_robj)) {
+  dir.create(output_dir_robj, recursive = TRUE)
+}
+
+# READ IN DATA:-----------------------------------------------------------------
 # Sample sheet containing paths to filtered bam files and narrowpeak files
-samples <- read.csv("data/meta_data/atacseq_e16/SampleSheet.csv")
+samples <- read.csv(input_file)
 
-# CREATE DBA OBJECT: -------------------------------------------------------------------------------------
+# CREATE DBA OBJECT: -----------------------------------------------------------
 dbObj <- dba(sampleSheet=samples)
 
-# COUNT READS: -------------------------------------------------------------------------------------------
-dbObj <- dba.count(dbObj, bUseSummarizeOverlaps=TRUE, summits = 75)
+# COUNT READS: -----------------------------------------------------------------
+dbObj <- dba.count(dbObj, 
+                   bUseSummarizeOverlaps=TRUE, 
+                   summits = 75)
 
 
-# NORMALIZE DATA: -------------------------------------------------------------------------------------------
+# NORMALIZE DATA: --------------------------------------------------------------
 # Normalization is based on sequencing depth by default
 dbObj <- dba.normalize(dbObj)
 
 # Save the DBA object after normalizing read counts for later use (making normalized counts plots)
-saveRDS(dbObj, file = "data/processed_data/atacseq_e16/r_objects/norm_read_counts_dbObj.rds")
+saveRDS(dbObj, 
+        file = file.path(output_dir_robj, 
+                         filename_normcounts)
+        )
 
-# ESTABLISH MODEL DESIGN AND CONTRAST: ---------------------------------------------------------------------
+# ESTABLISH MODEL DESIGN AND CONTRAST: -----------------------------------------
 # Make the "Control" condition the baseline or denominator of the default contrast.
 dbObj <- dba.contrast(dbObj, 
-                      reorderMeta = list(Condition = "Control"))
+                      reorderMeta = list(Condition = "Control")
+                      )
 
-# PERFORM DIFFERENTIAL ANALYSIS: ---------------------------------------------------------------------
+# PERFORM DIFFERENTIAL ANALYSIS: -----------------------------------------------
 # By default, using DESeq2 but can change to edgeR
 dbObj <- dba.analyze(dbObj)
 
-# SAVE DBA OBJECT FOR LATER USE: -------------------------------------------------------------------------
+# SAVE DBA OBJECT FOR LATER USE: -----------------------------------------------
 # Save the DBA object after differential analysis
-saveRDS(dbObj, file = "data/processed_data/atacseq_e16/r_objects/diffbind_dbObj.rds")
+saveRDS(dbObj, file = file.path(output_dir_robj, 
+                                filename_dbObj)
+        )
 
-print("Diffbind analysis done. RDS files saved in r_objects/")
+print(paste0("Diffbind analysis done. RDS files saved in ", output_dir_robj))
 
 
