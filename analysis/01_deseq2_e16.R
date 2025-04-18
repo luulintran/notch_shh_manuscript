b0fb656f06e3e5db032331cc3bf007586c1611ef
luulintran <luulitran@gmail.com>
2025-04-18 13:06:23 -0600

    Reorganized 01_deseq2_e16.R
    Reorganized script, moving code that loads libraries to the set_up_environment.R script. Created a config.R file for each fig, so also moved any input files, output dirs, filenames, etc to the config.R script so that everything can be modified here instead.

--- analysis/01_deseq2_e16.R
+++ analysis/01_deseq2_e16.R
@@ -1,31 +1,49 @@
-# SET UP
-library(DESeq2)
-library(tidyverse)
-library(dplyr)
-library(readr)
+# DEFINE FILES AND PATHS: ------------------------------------------------------
+# Path to the gene raw counts matrix
+input_cts <- "data/processed_data/rnaseq_e16/raw_counts/rnaseq_e16_raw_counts.tsv"
 
+# Path to the sample sheet file
+input_coldata <- "data/meta_data/rnaseq_e16/coldata.csv"
 
-# READ IN DATA:------------------------------------------------------------------------------------------
+# Path to output directory for r_objects
+output_dir_robj <- "data/processed_data/rnaseq_e16/r_objects"
+
+# filename for RDS file
+filename <- "deseq2_dds_e16.rds"
+
+# Make sure output directory exists, and if it doesn't, create one
+if (!dir.exists(output_dir_robj)) {
+  dir.create(output_dir_robj, recursive = TRUE)
+}
+
+# READ IN DATA:-----------------------------------------------------------------
 # raw gene counts data from star_salmon; remove gene_name column
-cts <- read.csv("data/processed_data/rnaseq_e16/gene_counts/rnaseq_16_gene_counts.tsv", sep= "\t", row.names = 1) # makes the rownames the first col
+cts <- read.csv(input_cts, 
+                sep= "\t", 
+                row.names = 1) # makes the rownames the first col
 cts <- as.data.frame(cts[, -c(1)]) # remove the gene_name col
 
 # sample metadata
-coldata <- read.csv("data/meta_data/rnaseq_e16/coldata.csv", row.names = 1)
+coldata <- read.csv(input_coldata, 
+                    row.names = 1)
 
-# CREATE DESEQ2 DATASET:---------------------------------------------------------------------------------
+# CREATE DESEQ2 DATASET:--------------------------------------------------------
 dds <- DESeqDataSetFromMatrix(countData = round(cts), 
                               colData = coldata, 
                               design = ~ condition)
 
-dds$condition <- relevel(dds$condition, ref = "control")
+dds$condition <- relevel(dds$condition, 
+                         ref = "control")
 
-# RUN DESEQ2 ANALYSIS: ----------------------------------------------------------------------------------
+# RUN DESEQ2 ANALYSIS: ---------------------------------------------------------
 dds <- DESeq(dds)
 
-# SAVE DESEQ2 OBJECT TO RDS FOR LATER: -------------------------------------------------------------------
-saveRDS(dds, file = "data/processed_data/rnaseq_e16/r_objects/deseq2_dds_e16.rds")
+# SAVE DESEQ2 OBJECT TO RDS FOR LATER: -----------------------------------------
+saveRDS(dds, 
+        file = file.path(output_dir_robj, 
+                         filename)
+        )
 
-print("DESeq2 analysis done. RDS file saved in r_objects/")
+print(paste0("DESeq2 analysis done. RDS file saved in ", output_dir_robj))
 
 
