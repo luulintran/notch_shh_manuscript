25abda1eff8196d9ce27a3dcd0345ea348fcdb17
luulintran <luulitran@gmail.com>
2025-04-18 13:10:04 -0600

    Update 02_fig3d_heatmap.R
    Reorganized script, moving code that loads libraries to the set_up_environment.R script. Created a config.R file for each fig, so also moved any input files, output dirs, filenames, etc to the config.R script so that everything can be modified here instead.

--- figures/fig3/02_fig3d_heatmap.R
+++ figures/fig3/02_fig3d_heatmap.R
@@ -1,25 +1,13 @@
-## Run after running 'analysis/02_diffbind_e16.R'
-
-library(Diffbind)
-
 # READ DBA OBJECT FOLLOWING DIFFBIND ANALYSIS FROM RDS FILE: -------------------
 # Make sure it's the one after running diffbind analysis
-dbObj.DB <- readRDS(
-  "data/processed_data/atacseq_e16/r_objects/diffbind_dbObj.rds")
+dbObj <- readRDS(rds_dbObj)
 
-# MAKE HEATMAP: ----------------------------------------------------------
-# Set file name and pdf settings for saving
-filename <- "figures/fig3/fig3d_heatmap.pdf"
-pdf(filename, width = 5, height = 6)
-
-# Choose heatmap colors
-hmap <- colorRampPalette(c("white", "#938fc2", "#40027e")) (n =100)
-
-# Plot heatmap
+# SAVE HEATMAP: ----------------------------------------------------------------
+pdf(file.path(output_dir_figures, filename_heatmap), width = 5, height = 6)
 readscores <- dba.plotHeatmap(dbObj, contrast = 1, correlations = FALSE,
                               scale = "row", colScheme = hmap)
 
 dev.off()
 
-print("Heatmap generated and saved in figures/fig3/")
+print(paste0(filename_heatmap, " generated and saved in ", output_dir_figures))
 
