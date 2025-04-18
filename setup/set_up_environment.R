7ef343800ba6d3942c1b28bb64361c431e1d33be
luulintran <luulitran@gmail.com>
2025-04-18 13:12:44 -0600

    Update set_up_environment.R
    Moved code to load libraries to set_up_environment.R script to do it all at once.

--- setup/set_up_environment.R
+++ setup/set_up_environment.R
@@ -23,16 +23,19 @@ required_directories <- c(
   "data/processed_data/atacseq_e16",
   "data/processed_data/rnaseq_e16",
   "data/processed_data/rnaseq_e17",
+  "data/processed_data/rnaseq_ctrl_e13",
   "data/processed_data/atacseq_e16/deeptools_output",
   "data/processed_data/atacseq_e16/diffbind_results_bed",
   "data/processed_data/atacseq_e16/filtered_bams",
   "data/processed_data/atacseq_e16/filtered_macs2",
   "data/processed_data/atacseq_e16/r_objects",
   "data/processed_data/homer_output",
-  "data/processed_data/rnaseq_e16/normalized_counts",
+  "data/processed_data/rnaseq_e16/raw_counts",
   "data/processed_data/rnaseq_e16/r_objects",
-  "data/processed_data/rnaseq_e17/normalized_counts",
+  "data/processed_data/rnaseq_e17/raw_counts",
   "data/processed_data/rnaseq_e17/r_objects/",
+  "data/processed_data/rnaseq_ctrl_e13/raw_counts",
+  "data/processed_data/rnaseq_ctrl_e13/r_objects/",
   "analysis",
   "figures",
   "preprocess",
@@ -46,5 +49,27 @@ for (dir in required_directories) {
     cat(paste0("Created directory: ", dir, "\n"))
   }
 }
+
+# Load libraries
+library(tidyverse)
+library(dplyr)
+library(readr)
+library(ggplot2)
+library(extrafont)
+library(pheatmap)
+library(data.table)
+library(reshape2)
+library(car)
+
+library(DESeq2)
+library(DiffBind)
+library(ChIPseeker)
+library(org.Mm.eg.db)
+library(TxDb.Mmusculus.UCSC.mm10.knownGene)
+library(clusterProfiler)
+library(RColorBrewer)
+
+# import Arial font
+font_import(pattern = "Arial", prompt = FALSE)
 
 cat("\nEnvironment setup complete.\n")
