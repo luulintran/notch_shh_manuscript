443eab15acfd6488224bb2d12d9868115ae7c03b
luulintran <luulitran@gmail.com>
2025-04-18 13:09:21 -0600

    Update 01_fig2b_pcaplot.R
    Reorganized script, moving code that loads libraries to the set_up_environment.R script. Created a config.R file for each fig, so also moved any input files, output dirs, filenames, etc to the config.R script so that everything can be modified here instead.

--- figures/fig2/01_fig2b_pcaplot.R
+++ figures/fig2/01_fig2b_pcaplot.R
@@ -1,35 +1,35 @@
-# Run after running 'analysis/01_deseq2_e16.R'
-
-# SET UP
-library(ggplot2)
-library(DESeq2)
-library(extrafont)
-
-# import Arial font
-font_import(pattern = "Arial", prompt = FALSE)
-
 # READ DDS OBJECT FROM RDS FILE: -----------------------------------------------
 dds <- readRDS(
-  file = "data/processed_data/rnaseq_e16/r_objects/deseq2_dds_e16.rds")
+  file = rds_deseq2_results
+  )
 
 # EXTRACT TRANSFORMED VALUES: --------------------------------------------------
-vsd <- vst(dds, blind=FALSE)
+vsd <- vst(dds, 
+           blind=FALSE)
 
 # CUSTOMIZE A PCA PLOT: --------------------------------------------------------
 # extract PCA data from vsd
-pcaData <- plotPCA(vsd, intgroup = "condition", returnData = TRUE)
+pcaData <- plotPCA(vsd, 
+                   intgroup = "condition", 
+                   returnData = TRUE)
 
 # get percentage of variance for first two principal components
-percentVar <- round(100 * attr(pcaData, "percentVar"))
+percentVar <- round(100 * attr(pcaData, 
+                               "percentVar")
+                    )
 
 # plot PCA
-pcaplot <- ggplot(pcaData, aes(x = PC1, y = PC2, color = condition)) +
+pcaplot <- ggplot(pcaData, aes(x = PC1, 
+                               y = PC2, 
+                               color = condition)) +
   geom_point(size = 4) +
   labs(title = "PCA",
        x = paste0("PC1[", percentVar[1], "%]"),
        y = paste0("PC2[", percentVar[2], "%]")) +
-  scale_color_manual(values = c("control" = "#76BAE0", "NICD" = "#B8396B")) +
-  theme_classic(base_family = "Arial", base_size = 12) +
+  scale_color_manual(values = c("control" = control_color, 
+                                "NICD" = mutant_color)) +
+  theme_classic(base_family = "Arial", 
+                base_size = 12) +
   theme(
     axis.line = element_blank(),  
     panel.border = element_rect(color = "black", fill = NA, linewidth = 1),  
@@ -38,10 +38,9 @@ pcaplot <- ggplot(pcaData, aes(x = PC1, y = PC2, color = condition)) +
   )
 
 # Set filename to save
-filename <- "figures/fig2/fig2b_pcaplot.pdf"
-pdf(filename, width = 3, height = 3)
+pdf(file.path(output_dir_figures, filename_pca), width = 3, height = 3)
 print(pcaplot)
 
 dev.off()
 
-print("PCA plot was generated and saved in figures/fig2/")
+print(paste0(filename_pca, " was generated and saved in ", output_dir_figures))
