7f3ead36d6dcb300ebdc23a8ab3c9d62f1091f59
luulintran <luulitran@gmail.com>
2025-04-18 13:09:58 -0600

    Update 01_fig3b_pcaplot.R
    Reorganized script, moving code that loads libraries to the set_up_environment.R script. Created a config.R file for each fig, so also moved any input files, output dirs, filenames, etc to the config.R script so that everything can be modified here instead.

--- figures/fig3/01_fig3b_pcaplot.R
+++ figures/fig3/01_fig3b_pcaplot.R
@@ -1,20 +1,15 @@
-# Run after running 'analysis/02_diffbind_e16.R'
-library(Diffbind)
-
 # READ DBA OBJECT FOLLOWING DIFFBIND ANALYSIS FROM RDS FILE: -------------------
 # Make sure it's the one after running diffbind analysis
-dbObj.DB <- readRDS(
-  "data/processed_data/atacseq_e16/r_objects/diffbind_dbObj.rds")
+dbObj <- readRDS(rds_dbObj)
 
 # MAKE PCA PLOT: ---------------------------------------------------------------
 # Set file name and pdf settings for saving
-filename <- "figures/fig3/fig3b_pcaplot.pdf"
-pdf(filename, width = 5, height = 5)
+pdf(file.path(output_dir_figures, filename_pca), width = 5, height = 5)
 
 # Perform PCA on all accessible chromatin sites
 dba.plotPCA(dbObj, DBA_CONDITION)
 
 dev.off()
 
-print("PCA plot generated and saved in figures/fig3/")
+print(paste0(filename_pca, " generated and saved in ", output_dir_figures))
 
